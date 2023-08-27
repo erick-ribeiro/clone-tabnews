3aa741fb9ac4b1cdd3f10095665072cacff3acf9
Erick Ribeiro <48537932+erick-ribeiro@users.noreply.github.com>
2023-08-27 19:01:57 +0000

    adicionando pagina produto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>teste</h1>
+  return <h1>Testando agora </h1>
 }
 
 export default Home
---
+++ pages/produtos/index.js
@@ -0,0 +1,5 @@
+function Produto() {
+    return <h1>Você localizadou uma pagina secreta</h1>
+  }
+  
+  export default Produto
