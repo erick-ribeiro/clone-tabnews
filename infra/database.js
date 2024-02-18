effe8193b374bd6c09e8ac14fd653d09c150dfea
Erick Ribeiro <48537932+erick-ribeiro@users.noreply.github.com>
2024-02-18 13:26:50 +0000

    torna o databese.js mais robusto para erros

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,27 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD
-    });
-    
-    await client.connect();
-    const result = await client.query(queryObject);
-    await client.end();
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
 
-    return result;
+  await client.connect();
+
+  try {
+    const result = await client.query(queryObject);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
+
+  return result;
 };
 
 export default {
-    query: query,
+  query: query,
 };
